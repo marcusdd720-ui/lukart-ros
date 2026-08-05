c79b2dcdbe91d1c96bc954328b9630a30e586da2
Łukasz Adamski <lukasz@example.com>
2026-08-05 18:31:31 +0200

    Clean CASE-0002: remove accidental test copies and pycache

--- cases/CASE-0002/original/test_bootstrap_1.py
+++
@@ -1,8 +0,0 @@
-from core.bootstrap import bootstrap
-from core.plugin_manager import PluginManager
-
-
-def test_bootstrap_returns_plugin_manager():
-    manager = bootstrap()
-
-    assert isinstance(manager, PluginManager)
